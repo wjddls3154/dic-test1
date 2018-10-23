9705c2e03fe64b0f411e4f2999518992342a8b22
kwackjihoon <chunjoo4@naver.com>
2018-10-23 17:34:21 +0900

    Initial Commit

---
+++ dic-test1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dic-test1
+//
+//  Created by dit on 2018. 10. 23..
+//  Copyright © 2018년 dit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
